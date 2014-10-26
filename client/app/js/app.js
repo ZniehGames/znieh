bf707c35be8f2496d5210ece0e2069e500fcf733
spyl94 <spyl94@gmail.com>
2014-10-26 18:46:52 +0100

    update vhosts

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,5 +1,1 @@
-"use strict";
-
-var app=angular.module("znieh",["ngRoute","restangular"]);
-
-app.config(["$interpolateProvider","$routeProvider","RestangularProvider",function(r,e,a){r.startSymbol("{[{"),r.endSymbol("}]}"),a.setBaseUrl("/api")}]);
+'use strict';

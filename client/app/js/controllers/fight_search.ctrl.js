ae8187538a313b37b3f1c657d6f1874f2452305d
FlorianLepot <florian.lepot@gmail.com>
2014-12-12 16:07:02 +0100

    Add selection team menu

--- client/app/js/controllers/fight_search.ctrl.js
+++ client/app/js/controllers/fight_search.ctrl.js
@@ -1,21 +1,17 @@
 'use strict';
 
 angular.module('znieh')
-    .controller('FightSearchCtrl', function ($scope, SocketService, toastr, $location, AuthenticationService) {
+    .controller('FightSearchCtrl', function ($scope, SocketService, toastr, $location, AuthenticationService, Restangular) {
 
     $scope.pool = 1;
     $scope.user = AuthenticationService.currentUser();
-    $scope.teams = [
-        {
-            id: 1,
-        },
-        {
-            id: 2,
-        },
-        {
-            id: 3,
-        }
-    ];
+    $scope.teams = Restangular
+                    .one('users', AuthenticationService.currentUser().id)
+                    .getList('teams')
+                    .then(function(teams) {
+                        $scope.teams = teams;
+                        $scope.selectedTeam = teams[0];
+                    });
 
     SocketService.on('searching match', function() {
         toastr.info('Recherche en cours', 'Informations');
@@ -28,4 +24,8 @@ angular.module('znieh')
     $scope.searchFight = function () {
         SocketService.emit('search match');
     };
+
+    $scope.select = function (index) {
+        $scope.selectedTeam = $scope.teams[index];
+    }
 });

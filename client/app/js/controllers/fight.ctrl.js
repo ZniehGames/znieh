9ae412223315733cd3e8a02d8e4e02cf589378fd
Aurélien <spyl94@gmail.com>
2015-01-21 19:04:58 +0100

    Phaser is now always loaded

--- client/app/js/controllers/fight.ctrl.js
+++ client/app/js/controllers/fight.ctrl.js
@@ -3,14 +3,16 @@
 angular.module('znieh')
 	.controller('FightCtrl', function ($scope, SocketService, $window, AuthenticationService) {
 
+  var game;
+
   SocketService.on('load user team', function(team) {
+      game = System.get('main')['default'].start();
+      game.io = SocketService;
+      game.side = $window.sessionStorage.side;
+      game.user = AuthenticationService.currentUser().username;
+      game.state.states.placement.team = team;
       $scope.team = team;
       console.log('load user team', team);
   });
-
-	this.fightPhaser = System.get('main')['default'].start();
-  this.fightPhaser.io = SocketService;
-  this.fightPhaser.side = $window.sessionStorage.side;
-  this.fightPhaser.user = AuthenticationService.currentUser().username;
 
 });

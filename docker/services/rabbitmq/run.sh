ac4da03db45fca8974b39c555ea41b2da7ff639b
Aurélien David <adavid@jolicode.com>
2015-04-21 11:44:49 +0200

    add rabbitmq

--- docker/services/rabbitmq/run.sh
+++ docker/services/rabbitmq/run.sh
@@ -2,17 +2,8 @@
 
 set -m
 
-if [ ! -f /.rabbitmq_password_set ]; then
-    /set_rabbitmq_password.sh
-fi
-
-# make rabbit own its own files
-chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
-
-echo "# Declaring mapping"
-
-# /usr/bin/rabbitmqadmin -p $RABBITMQ_PASS declare exchange name=email_registration type=direct auto_delete=false durable=true --vhost=/
-# /usr/bin/rabbitmqadmin -p $RABBITMQ_PASS declare queue name=email_registration auto_delete=false durable=true --vhost=/
-# /usr/bin/rabbitmqadmin -p $RABBITMQ_PASS declare binding source=email_registration destination=email_registration --vhost=/
+echo "Starting rabbitmq-server"
 
 /usr/sbin/rabbitmq-server
+
+echo "=> Done!"

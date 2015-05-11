d57bd757c90534495e7ccfb6c85f31b47b02605f
Aurélien David <adavid@jolicode.com>
2015-05-11 22:13:22 +0200

    twitter facebook

--- frontend/app/js/components/Contributing.js
+++ frontend/app/js/components/Contributing.js
@@ -15,8 +15,14 @@ const Contributing = React.createClass({
         <div className='col-md-8'>
           <h3>Participe au développement du jeu en proposant directement tes patchs !</h3>
           <p>Tu peux dès à présent télécharger les sources du jeu, et proposer tes améliorations aux développeurs.</p>
+          <a href='https://www.facebook.com/ZniehGames' target="_blank">
+            <img src={'images/facebook.png'} className='social'/>
+          </a>
           <a href='https://github.com/ZniehGames/znieh' target="_blank">
-            <img src={'images/github.png'} className='github'/>
+            <img src={'images/github.png'} className='social github'/>
+          </a>
+          <a href='https://twitter.com/ZniehGames' target="_blank">
+            <img src={'images/twitter.png'} className='social'/>
           </a>
         </div>
       </div>
